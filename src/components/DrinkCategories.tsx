24d81577c4b71b30c1e346160d9ca846b9b293f7
mghollasy5 <mghollasy5@gmail.com>
2025-11-17 13:18:41 -0600

    Update DrinkCategories.tsx

--- src/components/DrinkCategories.tsx
+++ src/components/DrinkCategories.tsx
@@ -241,8 +241,6 @@ export const RedWineTab = () => (
 );
 
 
-
-
 export const WhiteWineTab = () => (
   <TabsContent value="white-wine" className="space-y-8">
     <h2 className="font-playfair text-3xl font-bold text-cafe-maroon mb-4">Signature Cocktails</h2>
